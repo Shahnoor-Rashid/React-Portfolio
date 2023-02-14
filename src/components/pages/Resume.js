35a0a5b8deb75744570ea0922ed6efac5fd0dc86
ms rashid <msrashidgb@gmail.com>
2023-02-14 23:48:22 +0000

    resume path corrected.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,8 +8,8 @@ export default function Resume() {
       </header>
       <div className="content">
         <p>
-          Download my <a href="../../../public/Resume/ShahnoorResume.pdf">resume</a>
-        </p>
+          Download my <a href="https://github.com/Shahnoor-Rashid/React-Portfolio/raw/main/Public/Resume/ShahnoorResume.pdf" >Resume</a>
+          </p>
         <h5>Front-end Proficiencies</h5>
         <ul>
           <li>HTML</li>
